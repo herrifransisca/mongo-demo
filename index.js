0fc76a2cdd8e79264ae7765899a9d91b2925fb81
herrifransisca <herrifransisca@gmail.com>
2018-11-21 13:54:51 +0700

    07-08- Querying Documents

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ const courseSchema = mongoose.Schema({
   isPublished: Boolean
 });
 
+const Course = mongoose.model('Course', courseSchema);
+
 async function createCourse() {
-  const Course = mongoose.model('Course', courseSchema);
   const course = new Course({
-    name: 'Node.js Course',
+    name: 'Angular Course',
     author: 'Mosh',
-    tags: ['node', 'backend'],
+    tags: ['node', 'frontend'],
     isPublished: true
   });
 
@@ -26,4 +27,14 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+// createCourse();
+
+async function getCourses() {
+  const courses = await Course.find({ author: 'Mosh', isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourses();
